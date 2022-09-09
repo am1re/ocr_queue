e8dc137bd0f17a563cdbf7bcbc307dac7a54c6ef
Amir Abu Shamala <am1re344@gmail.com>
2022-09-09 22:28:17 +0300

    fix ocr job logging

--- app/jobs/ocr_process_job.rb
+++ app/jobs/ocr_process_job.rb
@@ -2,12 +2,12 @@ class OcrProcessJob < ApplicationJob
   queue_as :ocr_images
 
   rescue_from OcrProcessor::OcrProcessError do |e|
-    logger.info "[OcrProcessJob(#{job_id[0, 8]})] error processing job (#{e.message})"
+    Sneakers.logger.info "[OcrProcessJob(#{job_id[0, 8]})] error processing job (#{e.message})"
   end
 
   def perform(image_url)
     result = OcrProcessor.process image_url
-    logger.info "[OcrProcessJob(#{job_id[0, 8]})] result =>\n#{result}"
+    Sneakers.logger.info "[OcrProcessJob(#{job_id[0, 8]})] result =>\n#{result}"
     result
   end
 end
