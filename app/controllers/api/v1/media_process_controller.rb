081e3a27d5ac0a1d3f5401a7f3cb58243d93a31a
Amir Abu Shamala <am1re344@gmail.com>
2022-09-09 07:58:09 +0300

    file format validation, better api responses

--- app/controllers/api/v1/media_process_controller.rb
+++ app/controllers/api/v1/media_process_controller.rb
@@ -1,11 +1,28 @@
 class Api::V1::MediaProcessController < ApplicationController
+  rescue_from ActionController::ParameterMissing do |e|
+    render json: { message: e.message }, status: :bad_request
+  end
+
+  rescue_from Bunny::TCPConnectionFailedForAllHosts do
+    render json: { message: 'connection to RabbitMQ failed' }, status: :internal_server_error
+  end
+
   def receive
-    OcrProcessJob.perform_later(params[:fileUrl]) if valid_url? params[:fileUrl]
+    if valid_file_url? file_url_param
+      OcrProcessJob.perform_later file_url_param
+    else
+      render json: { message: 'invalid fileUrl, jpg/jpeg/png are supported' }, status: :bad_request
+    end
   end
 
   private
 
-  def valid_url?(url)
-    url =~ /\A#{URI::DEFAULT_PARSER.regexp[:ABS_URI]}\z/
+  def valid_file_url?(url)
+    url =~ /\A#{URI::DEFAULT_PARSER.regexp[:ABS_URI]}\z/ &&
+      File.extname(URI.parse(url).path) =~ /^(.png|.jpg|.jpeg)$/
+  end
+
+  def file_url_param
+    params.require(:fileUrl)
   end
 end
